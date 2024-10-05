2442316122315e74e21ed1548d1e3901a6a68378
Harshitha <chharshitha277@gmail.com>
2024-10-05 15:33:25 +0530

    "completed task 2 of backend js"

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const text1 = document.getElementById('text1');
+
+const button1 = document.getElementById('button1');
+button1.addEventListener('click', function() {
+    text1.innerText = 'Button 1 clicked!';
+});
+const text2 = document.getElementById('text2');
+
+const button2 = document.getElementById('button2');
+button2.addEventListener('click', function() {
+    text2.innerText = 'Button 2 clicked!';
+});
+
